df28d49760529fea73b403586a4d7a11420248ea
zwezdica <mzvezdic@gmail.com>
2025-07-18 10:30:34 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -19,8 +19,8 @@ mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB connected");
-    app.listen(process.env.PORT || 5000, () =>
-      console.log(`Server running on port ${process.env.PORT || 5000}`)
+    app.listen(process.env.PORT || 3000, () =>
+      console.log(`Server running on port ${process.env.PORT || 3000}`)
     );
   })
   .catch((err) => console.log(err));
